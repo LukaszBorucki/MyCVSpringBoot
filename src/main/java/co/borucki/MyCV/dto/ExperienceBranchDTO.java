fca73efcc60b74266d57a433255a1ad0f6cc2e7b
LukaszBorucki <lukasz@borucki.co>
2017-11-14 22:13:57 +0100

    Create AdditionalDTO, EducationDTO, ExperienceBranchDTO, ExperienceCompanyDTO, ExperiencePeriodDTO, ExperienceProjectDTO, ForeignLanguageDTO, HobbiesDTO, MailUserAuthorizationDTO, PersonalDataDTO, SkillDTO
    *Created AdditionalDTO,
    *Created EducationDTO,
    *Created ExperienceBranchDTO,
    *Created ExperienceCompanyDTO,
    *Created ExperiencePeriodDTO,
    *Created ExperienceProjectDTO,
    *Created ForeignLanguageDTO,
    *Created HobbiesDTO,
    *Created MailUserAuthorizationDTO,
    *Created PersonalDataDTO,
    *Created SkillDTO

---
+++ src/main/java/co/borucki/MyCV/dto/ExperienceBranchDTO.java
@@ -0,0 +1,40 @@
+package co.borucki.MyCV.dto;
+
+public class ExperienceBranchDTO {
+    private int id;
+    private String branchPl;
+    private String branchEn;
+
+    public ExperienceBranchDTO() {
+    }
+
+    public ExperienceBranchDTO(int id, String branchPl, String branchEn) {
+        this.id = id;
+        this.branchPl = branchPl;
+        this.branchEn = branchEn;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBranchPl() {
+        return branchPl;
+    }
+
+    public void setBranchPl(String branchPl) {
+        this.branchPl = branchPl;
+    }
+
+    public String getBranchEn() {
+        return branchEn;
+    }
+
+    public void setBranchEn(String branchEn) {
+        this.branchEn = branchEn;
+    }
+}
